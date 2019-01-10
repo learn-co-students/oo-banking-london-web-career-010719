ae4bb52732e4f22ffa10ac6621dc999a9d9b9e53
jchaselubitz <jchaselubitz@gmail.com>
2019-01-10 19:19:47 +0000

    Money transfer functionality for a bank. @HarryCopley was of no use what-so-ever. May god have mercy on his sole.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,40 @@
+require "pry"
+
 class Transfer
-  # your code here
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def valid?
+    self.sender.valid? && self.receiver.valid? && @sender.balance > @amount
+  end
+
+  def execute_transaction
+    if valid? && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if valid? && @status == "complete"
+    #  binding.pry
+      @receiver.balance -= @amount
+      @sender.balance += @amount
+      @status = "reversed"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
 end
